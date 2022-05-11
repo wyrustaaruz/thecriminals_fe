8b7000873e1e1d9719ff3c4c66704f2ad6994339
HASAN RIZA UZUNER <wyrustaaruz@gmail.com>
2022-05-11 12:40:00 +0300

    kulüpler eklendi

--- src/screens/home/Club.tsx
+++ src/screens/home/Club.tsx
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { Header, SubHeader, LastHeader } from "../../components";
+import { ClubList } from "../../components/ClubList";
+import { OwnClubList } from "../../components/OwnClubList";
+import { BuyClubList } from "../../components/BuyClubList";
 import { Loading, Text, View } from "../../components/PureComponents";
 import Colors from "../../constants/Colors";
 import Actions from "../../redux/actions";
@@ -11,18 +14,30 @@ export default function Club({ navigation }: any) {
   const characterInfo =
     useSelector((state: any) => state.homepageReducers.header) || {};
   const loading = useSelector((state: any) => state.commonReducers.loading);
+  const clubList = useSelector((state: any) => state.homepageReducers.clubList);
+  const ownClubList = useSelector(
+    (state: any) => state.homepageReducers.ownClubList
+  );
+  const jailStatus = useSelector(
+    (state: any) => state.homepageReducers.jailStatus
+  );
 
   const initHeader = async () => {
     await dispatch(Actions.homepageActions.GetHeader());
   };
 
-  const initRobberyList = async () => {
-    await dispatch(Actions.homepageActions.GetRobberyList());
+  const initClubs = async () => {
+    await dispatch(Actions.homepageActions.GetClubList());
+  };
+
+  const initOwnClubs = async () => {
+    await dispatch(Actions.homepageActions.GetOwnClubList());
   };
 
   useEffect(() => {
     initHeader();
-    initRobberyList();
+    initClubs();
+    initOwnClubs();
   }, []);
 
   return (
@@ -31,9 +46,22 @@ export default function Club({ navigation }: any) {
       <View>{Header(characterInfo, navigation)}</View>
       <View>{SubHeader(characterInfo, navigation)}</View>
       <View>{LastHeader(characterInfo)}</View>
+      <Text style={{ marginLeft: 15, marginTop: 15 }}>Bir Kulübe gir</Text>
       <View style={{ flex: 1 }}>
-        <Text>Kulüp Ekranı</Text>
+        {ClubList(navigation, clubList, jailStatus)}
       </View>
+
+      <Text style={{ marginLeft: 15, marginTop: 15 }}>
+        Sahip olduğun Kulüpler
+      </Text>
+      <View style={{ flex: 1 }}>
+        {OwnClubList(navigation, ownClubList, jailStatus)}
+      </View>
+
+      <Text style={{ marginLeft: 15, marginTop: 15 }}>
+        Satın Alabileceğin Kulüpler
+      </Text>
+      <View style={{ flex: 1 }}>{BuyClubList(clubList, jailStatus)}</View>
     </SafeAreaView>
   );
 }
